9bf3b18b15faebbb1b104f19bae9183ba140d353
Truitt Janney <truittjanney@MacBookPro.attlocal.net>
2025-07-31 13:23:27 -0500

    initial commit, fixed issues with file path, build failing fix, updated react component

---
+++ fastlane/App.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { Text, View} from 'react-native';
+
+// function HelloWorld()
+// {
+//         console.log("Hello, World!");
+
+//         return (
+//             <View>
+//                 <Text>Hello, World!</Text>
+//             </View>
+//         );
+// };
+
+// export default HelloWorld;
